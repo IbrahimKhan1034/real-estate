d066b7d7e69d2d3eb1396ee700df87506cb6583a
Your Name <ibrahim904072@gmail.com>
2025-07-31 01:02:47 +0500

    reslolve merge conflicts

--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -1,42 +1,29 @@
 import { createSlice, current } from "@reduxjs/toolkit";
 
 const initialState = {
-    currentUser: null,
-    error: null,
-    loading: false,
+currentUser: null,
+error: null,
+loading: false,
 };
 
 const userSlice = createSlice({
-    name:'user',
-    initialState,
-    reducers: {
-<<<<<<< HEAD
-        signInStart: (state) => {
-=======
-        signInStart: (state) => {
->>>>>>> e8ca77a72d1a2fa40e801419e3f878764916c325
-            state.loading = true;
-        },
-        signInSuccess: (state,action) => {
-            state.currentUser = action.payload;
-            state.loading = false;
-            state.error = null;
-        },
-        signInFailure: (state,action) => {
-<<<<<<< HEAD
-            state.error = action.payload;
-=======
-            state.currentUser = action.payload;
->>>>>>> e8ca77a72d1a2fa40e801419e3f878764916c325
-            state.loading = false;
+name: 'user',
+initialState,
+reducers: {
+    signInStart: (state) => {
+    state.loading = true;
+    },
+    signInSuccess: (state, action) => {
+    state.currentUser = action.payload;
+    state.loading = false;
+    state.error = null;
+    },
+    signInFailure: (state, action) => {
+    state.error = action.payload;
+    state.loading = false;
     }
 }
 });
 
 export const { signInStart, signInSuccess, signInFailure } = userSlice.actions;
-
-<<<<<<< HEAD
-export default userSlice.reducer; 
-=======
 export default userSlice.reducer;
->>>>>>> e8ca77a72d1a2fa40e801419e3f878764916c325
